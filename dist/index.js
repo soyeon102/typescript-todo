a4401dd79e1110b7f9651885f85628e82d9989f6
soyeon102 <csea9000@naver.com>
2022-11-11 00:12:26 +0900

    간단한 todo list 완성

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,40 @@
 var $btn = document.getElementById('btn');
 var $input = document.getElementById('todoInput');
 var $form = document.getElementById('todoForm');
+var $list = document.getElementById('todoList');
+var todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    var todosJSON = localStorage.getItem('todos');
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 function handleSubmit(e) {
     e.preventDefault();
-    console.log('submitted!');
+    var newTodo = {
+        text: $input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    $input.value = '';
+}
+function createTodo(todo) {
+    var newLi = document.createElement('li');
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLi.append(todo.text);
+    newLi.append(checkbox);
+    $list === null || $list === void 0 ? void 0 : $list.append(newLi);
 }
 $form === null || $form === void 0 ? void 0 : $form.addEventListener('submit', handleSubmit);
-$btn === null || $btn === void 0 ? void 0 : $btn.addEventListener('click', function () {
-    $input.value = '';
-});
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,51 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
 const $btn = document.getElementById('btn');
 const $input = document.getElementById('todoInput') as HTMLInputElement;
 const $form = document.getElementById('todoForm') as HTMLFormElement;
+const $list = document.getElementById('todoList');
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem('todos');
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+function saveTodos() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log('submitted!');
+  const newTodo: Todo = {
+    text: $input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+
+  saveTodos();
+  $input.value = '';
+}
+
+function createTodo(todo: Todo) {
+  const newLi = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener('change', function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+  newLi.append(todo.text);
+  newLi.append(checkbox);
+  $list?.append(newLi);
 }
 
 $form?.addEventListener('submit', handleSubmit);
-
-$btn?.addEventListener('click', function () {
-  $input.value = '';
-});
